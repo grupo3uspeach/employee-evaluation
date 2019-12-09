245ee04d04edb323010286c3f0c9fbc08baa7dff
grupo3uspeach <grupo3uspeach@gmail.com>
2019-12-09 20:14:49 -0300

    Project Renewal

--- Avaliacao/src/main/java/br/com/avaliacao/repository/CampoAvaliacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.avaliacao.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.avaliacao.entity.CampoAvaliacao;
-
-public interface CampoAvaliacaoRepository extends JpaRepository<CampoAvaliacao, Integer> {
-
-}
